56ef6c3609271b7b205574d3a176cac603c5c8eb
Suprith S A <suprithseegepalya@gmail.com>
2026-02-22 22:02:52 +0530

    Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while(left < right) {
+            if(s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
+
