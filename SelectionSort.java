154d9b060d966be6bb5c62b02e931d77e105895a
Suprith S A <suprithseegepalya@gmail.com>
2026-03-02 23:14:35 +0530

    SelectionSort

---
+++ SelectionSort.java
@@ -0,0 +1,25 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        // Print sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
