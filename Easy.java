35d51ea02ce181e874f5b46f9f7c3af5880a7eab
Suprith S A <suprithseegepalya@gmail.com>
2026-04-20 22:30:26 +0530

    Easy.java

---
+++ Easy.java
@@ -0,0 +1,14 @@
+public class EasyArray {
+    public static void main(String[] args) {
+        
+        // 1. Create a simple array with exactly 3 numbers
+        int[] myNumbers = {10, 20, 30};
+
+        // 2. Print them out manually. 
+        // Remember: Arrays always start counting at 0!
+        System.out.println("The first number is: " + myNumbers[0]);
+        System.out.println("The second number is: " + myNumbers[1]);
+        System.out.println("The third number is: " + myNumbers[2]);
+        
+    }
+}
