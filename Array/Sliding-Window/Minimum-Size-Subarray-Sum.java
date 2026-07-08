da77e2cfac8f58d055f6770763368f08c10e5335
Suprith S A <suprithseegepalya@gmail.com>
2026-07-08 18:39:42 +0530

    Minimum-Size-Subarray-Sum

---
+++ Array/Sliding-Window/Minimum-Size-Subarray-Sum.java
@@ -0,0 +1,21 @@
+
+public class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left =0;
+        int minlen = Integer.MAX_VALUE;
+        int sum =0;
+
+        for(int right=0;right<nums.length;right++){
+            sum += nums[right];
+
+            while(sum>=target){
+                minlen=Math.min(minlen,right-left+1);
+                sum -=nums[left];
+                left++;
+            }
+        }
+        return minlen==Integer.MAX_VALUE?0:minlen;
+    }
+} {
+    
+}
