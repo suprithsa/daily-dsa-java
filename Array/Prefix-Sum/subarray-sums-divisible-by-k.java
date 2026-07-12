f0bfb85ac2489100974ef16d0bf9c4110ec08cea
Suprith S A <suprithseegepalya@gmail.com>
2026-07-12 22:59:47 +0530

    subarray-sums-divisible-by-k

---
+++ Array/Prefix-Sum/subarray-sums-divisible-by-k.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            prefixSum += num;
+
+            int remainder = ((prefixSum % k) + k) % k;
+
+            if (map.containsKey(remainder)) {
+                count += map.get(remainder);
+            }
+
+            map.put(remainder, map.getOrDefault(remainder, 0) + 1);
+        }
+
+        return count;
+    }
+}
