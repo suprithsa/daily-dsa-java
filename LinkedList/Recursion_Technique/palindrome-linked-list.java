bb465df0b4dd96f239656b43fe01a4ce3d883356
Suprith S A <suprithseegepalya@gmail.com>
2026-09-24 22:05:38 +0530

    palindrome-linked-list

---
+++ LinkedList/Recursion_Technique/palindrome-linked-list.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    ListNode left;
+
+    public boolean isPalindrome(ListNode head) {
+        left = head;
+        return check(head);
+    }
+
+    private boolean check(ListNode right) {
+
+        
+        if (right == null) {
+            return true;
+        }
+
+        
+        if (!check(right.next)) {
+            return false;
+        }
+
+       
+        if (left.val != right.val) {
+            return false;
+        }
+
+       
+        left = left.next;
+
+        return true;
+    }
+}
